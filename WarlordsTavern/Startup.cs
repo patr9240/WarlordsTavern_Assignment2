08fa2108974bbc6268c554435e97ed8634883ea7
patr9240 <patr9240@gmail.com>
2016-07-28 02:47:42 -0400

    Add project files.

---
+++ WarlordsTavern/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WarlordsTavern.Startup))]
+namespace WarlordsTavern
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
